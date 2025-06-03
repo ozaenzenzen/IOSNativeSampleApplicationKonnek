d9105f6a898a67207917c6847d4bffb21852ef4a
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-06-03 15:53:19 +0700

    update: sample implementation simple

--- SampleApplication4/SampleApplication4App.swift
+++ SampleApplication4/SampleApplication4App.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import SampleLibrary2
 
 @main
 struct SampleApplication4App: App {
+    public static let shared = BridgeUIManager()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear() {
+                    SampleApplication4App.shared.initFunction(
+                        clientId: "b699182d-5ff0-4161-b649-239234ff9cb9",
+                        clientSecret: "1dc8e065-2915-4b4e-8df2-45040e8314bd",
+                        flavor: "staging"
+                    )
+                    
+                    // Add draggable button to window
+                    SampleApplication4App.shared.showFloatingButton()
+                }
         }
     }
 }

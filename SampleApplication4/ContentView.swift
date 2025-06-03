422a4fd40327014ce76dc758f805221526987895
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-06-03 15:43:37 +0700

    init

---
+++ SampleApplication4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleApplication4
+//
+//  Created by Fauzan Akmal Mahdi on 03/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleApplication4/SampleApplication4App.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApplication4App.swift
+//  SampleApplication4
+//
+//  Created by Fauzan Akmal Mahdi on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleApplication4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
